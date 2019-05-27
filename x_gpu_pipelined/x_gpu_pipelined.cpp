418ee47ac92e8e5f99bb27ff41e6404d81100586
gcallanan <gcallanan@ska.ac.za>
2019-05-27 13:23:43 +0000

    Updated global definitions comments

---
+++ x_gpu_pipelined/x_gpu_pipelined.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file x_gpu_pipelined.cpp
+ *
+ * @brief This is the main file for the MeerKAT GPU X-ENgine program. It launches all the threads in the pipeline
+ *
+ * @author Gareth Callanan
+ *
+ */
+
+#include "tbb/flow_graph.h"
+
+/// \brief  Main function, launches all threads in the pipeline, exits when all other threads close.
+/// \param  argc An integer argument count of the command line arguments
+/// \param  argv An argument vector of the command line arguments
+/// \return an integer 0 upon exit success
+int main(int argc, char** argv){
+    return 0;
+}
